18863863ecab9c4a0bec469a210c0f999703ed3e
tochchaskypark <tochchka@rambler.ru>
2017-06-22 22:00:11 +0300

    constructor page

--- js/common.js
+++ js/common.js
@@ -2,17 +2,21 @@
 
 jQuery(document).ready(function() {
 
-    // слайдер (настройки тут http://kenwheeler.github.io/slick/)
-    /*$('.share-slider').slick({
-        infinite: true,
-        dots: true,
-        fade: false,
-        arrows: false,
-        pauseOnFocus: false,
-        speed: 400,
-        autoplaySpeed: 6000,
-        autoplay: true
-    });*/
+    $('.popup-with-move-anim').magnificPopup({
+        type: 'inline',
+
+        fixedContentPos: false,
+        fixedBgPos: true,
+
+        overflowY: 'auto',
+
+        closeBtnInside: true,
+        preloader: false,
+
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-slide-bottom'
+    }); //fade-popup
 
     // календарь
     $("input.tcal").click(function() {
